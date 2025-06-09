70f115004a9785b72481b6b785e2135046729072
Bwire Gilbert <williambwire84@gmail.com>
2025-06-09 13:38:06 +0300

    bela.py This is alittle bit modified

--- bela.py
+++ bela.py
@@ -1,24 +1,26 @@
 import os 
 os.system("cls")
 
-class person:
+class Person:
     def __init__(self ,fname, lname):
         self.fname = fname
         self.lname =lname
-    def printName(self):
+    def __str__(self):
         return f'{self.fname} {self.lname} ' 
 
-class student(person):
+class Student(Person):
+    
     def __init__(self,fname,lname,year,course,mark):
         super().__init__(fname,lname)
         self.Graduationyear = year
         self.course = course
         self.mark = mark
-    def printName(self):
-        return super().printName()
+    
     def __str__(self):
-        return ( f' Name: {self.printName()},  Year: {self.Graduationyear}, ' f'Course: {self.course},  Mark: {self.mark}')   
-student1 = student('Akullu Bella ','Prossy',2009,'Information Technology',60) 
+        return ( f' Name: {super().__str__()},  Year: {self.Graduationyear}, ' f'Course: {self.course},  Mark: {self.mark}') 
+      
+student1 = Student('Akullu Bella ','Prossy',2009,'Information Technology',60) 
+
 print(student1)          
     
 
